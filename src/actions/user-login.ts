2ba07f68daaef7ffb186c96956404b93827f349e
Jean Carlos <jeancarlos.na@hotmail.com>
2024-12-11 00:11:28 -0300

    hotfix: login

--- src/actions/user-login.ts
+++ src/actions/user-login.ts
@@ -5,9 +5,10 @@ import ProdConfig from "@/app/api/config/prod_config";
 import { cookies } from 'next/headers';
 
 export async function loginUser(data: FormData | null): Promise<any> {
-  const environment = new ProdConfig();
+  const _environment = new ProdConfig();
   const cookieStore = await cookies();
-  console.log(data)
+  // console.log(data)
+
   if (!data) {
     throw new Error("Os dados enviados são inválidos.");
   }
@@ -30,7 +31,7 @@ export async function loginUser(data: FormData | null): Promise<any> {
   };
 
   const reqOptions = {
-    url: `${environment.API_URL}/login`,
+    url: `${_environment.API_URL}/login`,
     method: "POST",
     headers: headersList,
     data: bodyContent,
@@ -43,8 +44,11 @@ export async function loginUser(data: FormData | null): Promise<any> {
 
     if (response.status === 200) {
       cookieStore.set("token", response.data.objAuth.token, {
-        httpOnly: true,
-        maxAge: 24 * 60 * 60,
+        httpOnly: true, // Não acessível via JavaScript (protege contra XSS)
+        secure: process.env.NEXT_PUBLIC_DEBUG === "production", // Garante que o cookie seja seguro apenas em produção
+        maxAge: 24 * 60 * 60, // 1 dia de expiração
+        sameSite: "strict", // Protege contra ataques CSRF
+        path: '/',  // Torna o cookie acessível globalmente no domínio
       });
     }
 
