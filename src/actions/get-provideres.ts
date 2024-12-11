efe89d53d07d1e5b84d97f4b37e7fdfe61e449d8
Jean Carlos <jeancarlos.na@hotmail.com>
2024-12-11 00:11:04 -0300

    feature: end-point para retornar todos os provedores

--- src/actions/get-provideres.ts
+++ src/actions/get-provideres.ts
@@ -1,7 +1,31 @@
 "use server";
 
-const teste = async (_: unknown, formData: FormData) => {
-    console.log("Teste", Object.fromEntries(formData))
-}
+import ProdConfig from "@/app/api/config/prod_config";
+import axios from "axios";
+import { cookies } from "next/headers";
 
-export default teste;
+export async function getProviders() {
+    const _environment = new ProdConfig();
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token");
+
+    const headersList = {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token!.value}`
+    };
+
+    const reqOptions = {
+        url: `${_environment.API_URL}/providers`,
+        method: "GET",
+        headers: headersList
+    };
+
+    try {
+        const response = await axios.request(reqOptions);
+        console.log(response.data);
+        return response.data;
+    } catch (error) {
+        console.error("Erro ao obter provedores:", error);
+    }
+}
