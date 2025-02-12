63a3392478acf60eb7ac212ed12841eda58f202c
Jean Carlos <jeancarlos.na@hotmail.com>
2025-02-12 00:02:51 -0300

    hotfix: pagina hotspot

---
+++ src/app/(pages)/provider/hotspot_list/[...id]/[...slug]/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+
+import SideBar from "@/app/components/_ui/sidebar/Sidebar";
+import React from "react";
+
+const HotspotPage = ({ params }: any) => {
+  const resolvedParams = React.use<any>(params);
+  const slug = resolvedParams.slug[0];
+  const slugID = resolvedParams.slug[1];
+
+  return (
+    <SideBar>
+      <div className="sidebar--container">
+        <h1>Provider: {slug}</h1>
+        {/* Renderize mais conteúdo com base no slug aqui */}
+      </div>
+    </SideBar>
+  );
+};
+
+export default HotspotPage;
