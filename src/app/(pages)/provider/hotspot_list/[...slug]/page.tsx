258da9377825d6e9ac39a805b3e512d7e0b56d34
Jean Carlos <jeancarlos.na@hotmail.com>
2025-02-12 00:36:45 -0300

    feature: função pagina que lista o hotspot

--- src/app/(pages)/provider/hotspot_list/[...slug]/page.tsx
+++ src/app/(pages)/provider/hotspot_list/[...slug]/page.tsx
@@ -1,17 +1,38 @@
 "use client";
 
+import { getAllHotspots } from "@/actions/get-all-hotspots";
 import SideBar from "@/app/components/_ui/sidebar/Sidebar";
-import React from "react";
+import { IHotspot } from "@/app/interface/IUsersConnected";
+import React, { useEffect, useState } from "react";
 
 const HotspotsPage = ({ params }: any) => {
+  const [allHotSpots, setAllHotspots] = useState<IHotspot[]>([]);
+  const [error, setError] = useState<any>();
+  const [loading, setLoading] = useState<boolean>();
   const resolvedParams = React.use<any>(params);
   const slug = resolvedParams.slug[0];
-//   const slugID = resolvedParams.slug[1];
+  //   const slugID = resolvedParams.slug[1];
 
+  useEffect(() => {
+    const fetchHotspots = async () => {
+      const _getAllHotspots = await getAllHotspots(slug);
+      try {
+        setAllHotspots(_getAllHotspots);
+      } catch (error) {
+        setError("Erro ao carregar os hotspots");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchHotspots();
+  }, []);
+  console.log(allHotSpots);
   return (
     <SideBar>
       <div className="sidebar--container">
-        <h1>Hotspot List: {slug}</h1>
+        {allHotSpots.map((item) => {
+          return <h1 key={item.id}>Hotspot List: {item.nome}</h1>;
+        })}
         {/* Renderize mais conteúdo com base no slug aqui */}
       </div>
     </SideBar>
