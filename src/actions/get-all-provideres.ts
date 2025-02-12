975f38e999c872b12589aa9407d8faa54b68f74a
Jean Carlos <jeancarlos.na@hotmail.com>
2025-02-11 22:28:03 -0300

    feature: inserido fetch para todos os provedores

---
+++ src/actions/get-all-provideres.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import ProdConfig from "@/app/api/config/prod_config";
+import axios from "axios";
+import { cookies } from "next/headers";
+
+export async function getAllProviders() {
+    const _environment = new ProdConfig();
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token");
+
+    const headersList = {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token!.value}`
+    };
+
+    const reqOptions = {
+        url: `${_environment.API_URL}/providers`,
+        method: "GET",
+        headers: headersList
+    };
+
+    try {
+        const response = await axios.request(reqOptions);
+        // console.log(response.data);
+        return response.data;
+    } catch (error) {
+        console.error("Erro ao obter provedores:", error);
+    }
+}
