97042818fb1a9267ebc60d9cbc127e17acf21b66
Jean Carlos <jeancarlos.na@hotmail.com>
2025-03-16 19:21:58 -0300

    feature: pegando informações do provedor

---
+++ src/actions/get-provider.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import ProdConfig from "@/app/api/config/prod_config";
+import IProvider from "@/app/interface/IProviders";
+import axios from "axios";
+import { cookies } from "next/headers";
+
+export async function getProvider(id: number | string) {
+    const _environment = new ProdConfig();
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token");
+
+    const headersList = {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token!.value}`
+    };
+
+    const reqOptions = {
+        url: `${_environment.API_URL}/provider/${id}`,
+        method: "GET",
+        headers: headersList,
+    };
+
+    try {
+        const response = await axios.request(reqOptions);
+        // console.log(response.data)
+        return response.data
+    } catch (error) {
+        console.error("Erro ao buscar provedores", error)
+    }
+}
