62b1745dac98d05ba91cc2c34a4aa3d1b48d6913
Jean Carlos <jeancarlos.na@hotmail.com>
2025-03-16 19:22:18 -0300

    feature: adicionado fetch para update de provedor, falta implementar na api

---
+++ src/actions/update-provider.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import ProdConfig from "@/app/api/config/prod_config";
+import IProvider from "@/app/interface/IProviders";
+import axios from "axios";
+import { cookies } from "next/headers";
+
+export async function updateProvider(formData: IProvider) {
+    const _environment = new ProdConfig();
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token");
+
+    const headersList = {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token!.value}`
+    };
+
+    const reqOptions = {
+        url: `${_environment.API_URL}/provider/update`,
+        method: "GET",
+        headers: headersList,
+        data: formData
+    };
+
+    try {
+        const response = await axios.request(reqOptions);
+        return {
+            response: response.data,
+            status: response.status
+        }
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            return {
+                status: error.response?.status || 500,
+                data: error.response?.data || "Erro ao salvar provedor",
+            };
+        }
+        return {
+            status: 500,
+            data: "Erro desconhecido ao salvar provedor",
+        };
+    };
+}
